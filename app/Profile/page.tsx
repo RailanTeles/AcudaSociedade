d02bde5eb77781d5a1d5624fa9c57ca0b2abdd1c
Railan Teles <railan.oliveira.ofc@gmail.com>
2025-06-10 21:24:00 -0300

    [Railan] Página Profile: Dados do Usuário completos

--- app/Profile/page.tsx
+++ app/Profile/page.tsx
@@ -47,8 +47,8 @@ export default function Profile() {
       </div>
 
       {/* Página Do Perfil */}
-      <div className="w-full flex justify-center">
-        <div className="w-[90%] md:h-[25%] h-[45%] my-[1%] flex items-center relative">
+      <div className="w-full flex items-center flex-col">
+        <div className="w-[90%] md:h-[25%] h-[15%] my-[1%] flex items-center relative">
           <img
             src="Images/person-placeholder.png"
             alt=""
@@ -56,15 +56,42 @@ export default function Profile() {
           />
           <div className="flex flex-col gap-0.5">
             <p className="font-bold md:text-[40px]">nomePerfil</p>
-            <p className="font-bold md:text-[20px] text-gray-700">@nome_arroba</p>
+            <p className="font-bold md:text-[20px] text-gray-700">
+              @nome_arroba
+            </p>
           </div>
           <button
             type="button"
-            className="md:text-2xl text-[20px] text-black font-bold bg-white md:w-[20%] md:h-[40%] h-[50%] w-[25%] rounded-4xl border-2 cursor-pointer transform transition duration-300 hover:scale-110 hover:bg-[#1E3A8A] hover:text-white absolute right-0"
+            className="md:text-2xl text-[15px] text-black font-bold bg-white md:w-[20%] md:h-[40%] h-[50%] w-[25%] rounded-4xl border-2 cursor-pointer transform transition duration-300 hover:scale-110 hover:bg-[#1E3A8A] hover:text-white absolute right-0"
           >
             Editar
           </button>
         </div>
+        <div className="w-[90%] md:min-h-[15%] min-h-[30%] my-[2%] flex relative">
+          <p className="md:text-[14px] text-[12px] text-justify">
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla
+            facilisi. Sed vitae ligula id justo sodales varius. Fusce fermentum,
+            lacus at fermentum luctus, nisl purus fringilla enim, vel
+            scelerisque lorem urna ut libero. Suspendisse potenti. Aenean at
+            metus at augue laoreet scelerisque. Integer et justo at lorem
+            efficitur tincidunt. Curabitur nec magna ut libero tincidunt
+            faucibus.Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+            Nulla facilisi. Sed vitae ligula id justo sodales varius. Fusce
+            fermentum, lacus at fermentum luctus, nisl purus fringilla enim, vel
+            scelerisque lorem urna ut libero. Suspendisse potenti. Aenean at
+            metus at augue laoreet scelerisque. Integer et justo at lorem
+            efficitur tincidunt. Curabitur nec magna ut libero tincidunt
+            faucibus.
+          </p>
+        </div>
+        <h1 className="font-bold md:text-[40px] text-2xl my-[3%]">
+          Minhas Informações
+        </h1>
+        <hr className="w-full border-[#274CB4] border-1" />
+        <h1 className="font-bold md:text-[40px] text-2xl my-[3%]">
+          Minhas Postagens
+        </h1>
+        <hr className="w-full border-[#274CB4] border-1" />
       </div>
 
       {/* Modal */}
