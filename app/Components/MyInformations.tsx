f002a121338ed0d5017065f9458cbf53dd8d1140
RailanTeles <135670809+RailanTeles@users.noreply.github.com>
2025-06-13 02:44:18 -0300

    [Railan] Modal para alterar informações iniciado
    Falta:
    Terminar esse modal;
    Ver uma forma de as infos aparecerem

--- app/Components/MyInformations.tsx
+++ app/Components/MyInformations.tsx
@@ -1,49 +1,83 @@
-export default function MyInformations() {
+"use client";
+
+type InfoProps = {
+  email: string | null;
+  insta: string;
+  tel: string | null;
+  doacao: string | null;
+  loc: string | null;
+};
+
+export default function MyInformations({
+  email,
+  insta,
+  tel,
+  doacao,
+  loc,
+}: InfoProps) {
   return (
     <div className="w-[90%] my-[2%] flex md:flex-row flex-col relative justify-evenly md:items-baseline items-center md:gap-0 flex-wrap">
-      <div className="flex flex-col items-center w-[30%] gap-[10px] min-w-[250px] overflow-hidden my-[1%] md:border-0 border-t-2 border-[#274CB4]">
-        <h1 className="font-bold md:text-[25px] text-[20px] mb-[1%]">
-          Redes Sociais
-        </h1>
-        <a href="mailto:ong@gmail.com" className="flex items-center gap-[10px]" target="_blank">
-          <img src="Images/email.png" alt="" />
-          <p className="font-bold md:text-[14px] text-[12px]">ong@gmail.com</p>
-        </a>
-        <a
-          href="https://www.instagram.com/railanteles12"
-          className="flex items-center gap-[10px]"
-          target="_blank"
-        >
-          <img src="Images/instagram.png" alt="" />
-          <p className="font-bold md:text-[14px] text-[12px]">
-            ong@gmail.comdsfsdgsdfsdgfd
-          </p>
-        </a>
-        <a
-          href="https://wa.me/75982062321"
-          className="flex items-center gap-[10px]"
-          target="_blank"
-        >
-          <img src="Images/whatsapp.png" alt="" />
-          <p className="font-bold md:text-[14px] text-[12px]">75982062321</p>
-        </a>
-      </div>
-
-      <div className="flex flex-col items-center w-[30%] gap-[10px] min-w-[250px] my-[1%] overflow-hidden md:border-0 border-t-2 border-[#274CB4]">
-        <h1 className="font-bold md:text-[25px] text-[20px] mb-[1%]">Doação</h1>
-        <div className="flex items-center gap-[10px]">
-          <img src="Images/pix.png" alt="" />
-          <p className="font-bold md:text-[14px] text-[12px]">ong@gmail.com</p>
+      {email != null || insta != "" || tel != null ? (
+        <div className="flex flex-col items-center w-[35%] gap-[10px] min-w-[250px] overflow-hidden my-[1%] md:border-0 border-t-2 border-[#274CB4]">
+          <h1 className="font-bold md:text-[25px] text-[20px] mb-[1%]">
+            Redes Sociais
+          </h1>
+          {email != null ? (
+            <a
+              href={`mailto:${email}`}
+              className="flex items-center gap-[10px]"
+              target="_blank"
+            >
+              <img src="Images/email.png" alt="" />
+              <p className="font-bold md:text-[14px] text-[10px]">{email}</p>
+            </a>
+          ) : null}
+          {email != "" ? (
+            <a
+              href={insta}
+              className="flex items-center gap-[10px]"
+              target="_blank"
+            >
+              <img src="Images/instagram.png" alt="" />
+              <p className="font-bold md:text-[14px] text-[12px]">{insta}</p>
+            </a>
+          ) : null}
+          {tel != null ? (
+            <a
+              href={`https://wa.me/${tel}`}
+              className="flex items-center gap-[10px]"
+              target="_blank"
+            >
+              <img src="Images/whatsapp.png" alt="" />
+              <p className="font-bold md:text-[14px] text-[12px]">{tel}</p>
+            </a>
+          ) : null}
         </div>
-      </div>
+      ) : null}
 
-      <div className="flex flex-col items-center w-[30%] gap-[10px] min-w-[250px] my-[1%] overflow-hidden md:border-0 border-t-2 border-[#274CB4]">
-        <h1 className="font-bold md:text-[25px] text-[20px] mb-[1%]">Localização</h1>
-        <div className="flex items-center gap-[10px]">
-          <img src="Images/marcador.png" alt="" />
-          <p className="font-bold md:text-[14px] text-[12px]">Fraga Maia</p>
+      {doacao && (
+        <div className="flex flex-col items-center w-[30%] gap-[10px] min-w-[250px] my-[1%] overflow-hidden md:border-0 border-t-2 border-[#274CB4]">
+          <h1 className="font-bold md:text-[25px] text-[20px] mb-[1%]">
+            Doação
+          </h1>
+          <div className="flex items-center gap-[10px]">
+            <img src="Images/pix.png" alt="" />
+            <p className="font-bold md:text-[14px] text-[12px]">{doacao}</p>
+          </div>
         </div>
-      </div>
+      )}
+
+      {loc && (
+        <div className="flex flex-col items-center w-[30%] gap-[10px] min-w-[250px] my-[1%] overflow-hidden md:border-0 border-t-2 border-[#274CB4]">
+          <h1 className="font-bold md:text-[25px] text-[20px] mb-[1%]">
+            Localização
+          </h1>
+          <div className="flex items-center gap-[10px]">
+            <img src="Images/marcador.png" alt="" />
+            <p className="font-bold md:text-[14px] text-[12px]">{loc}</p>
+          </div>
+        </div>
+      )}
     </div>
   );
 }
--- app/Profile/page.tsx
+++ app/Profile/page.tsx
@@ -1,17 +1,38 @@
 "use client";
 
 import { useRouter } from "next/navigation";
-import { useEffect, useState } from "react";
+import { use, useEffect, useState } from "react";
 import Loading from "../Components/Loading";
 import NavBar from "../Components/NavBar";
 import AddPost from "../Components/AddPost";
 import { useAuth } from "../Context/AuthContext";
 import MyInformations from "../Components/MyInformations";
+import EditProfile from "../Components/EditProfile";
 
 export default function Profile() {
+  // Configurações
   const { isLogged } = useAuth();
   const router = useRouter();
+
+  // Dados
+  const [image, setImage] = useState<string>("Images/person-placeholder.png"); // Esse vai ser o padrão caswo o usuário não tenha foto
+  const [userExi, setUserExi] = useState("nomePerfil");
+  const [username, setUsername] = useState("nome_arroba");
+  const [email, setEmail] = useState("");
+  const [desc, setDesc] = useState<string | null>(
+    "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla facilisi. Sed vitae ligula id justo sodales varius. Fusce fermentum, lacus at fermentum luctus, nisl purus fringilla enim, vel scelerisque lorem urna ut libero. Suspendisse potenti. Aenean at metus at augue laoreet scelerisque. Integer et justo at lorem efficitur tincidunt. Curabitur nec magna ut libero tincidunt faucibus.Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla facilisi. Sed vitae ligula id justo sodales varius. Fusce fermentum, lacus at fermentum luctus, nisl purus fringilla enim, vel scelerisque lorem urna ut libero. Suspendisse potenti. Aenean at metus at augue laoreet scelerisque. Integer et justo at lorem efficitur tincidunt. Curabitur nec magna ut libero tincidunt faucibus."
+  );
+  const [emailExibi, setEmailExibi] = useState<string | null>(null);
+  const [insta, setInsta] = useState<string>(
+    "https://www.instagram.com/railanteles12"
+  );
+  const [tel, setTel] = useState<string | null>(null);
+  const [doacao, setDoacao] = useState<string | null>("ong@gmail.com");
+  const [loc, setLoc] = useState<string | null>("Fraga Maia");
+
+  // Modais
   const [modal, setModal] = useState(false);
+  const [modalEdit, setModalEdit] = useState(false);
 
   // Métodos
   const AbrirModalPost = () => {
@@ -19,6 +40,14 @@ export default function Profile() {
       setModal(true);
     } else {
       setModal(false);
+    }
+  };
+
+  const AbrirModalEdit = () => {
+    if (modalEdit == false) {
+      setModalEdit(true);
+    } else {
+      setModalEdit(false);
     }
   };
 
@@ -45,53 +74,46 @@ export default function Profile() {
       <div className="lg:w-[80%] md:w-[75%] w-full flex items-center flex-col lg:ml-[22%] md:ml-[27%] md:mt-[0%] mt-[48%]">
         <div className="w-[90%] md:h-[20%] h-[10%] my-[1%] flex items-center relative">
           <img
-            src="Images/person-placeholder.png"
+            src={image}
             alt=""
-            className="rounded-full border-2 h-full md:mr-[6%] mr-[4%]"
+            className="rounded-full border-2 h-[85%] md:mr-[6%] mr-[4%]"
           />
           <div className="flex flex-col gap-0.5">
-            <p className="font-bold md:text-[40px]">nomePerfil</p>
+            <p className="font-bold md:text-[40px]">{userExi}</p>
             <p className="font-bold md:text-[20px] text-gray-700">
-              @nome_arroba
+              @{username}
             </p>
           </div>
           <button
             type="button"
             className="md:text-2xl text-[15px] text-black font-bold bg-white md:w-[20%] md:h-[40%] h-[50%] w-[25%] rounded-4xl border-2 cursor-pointer transform transition duration-300 hover:scale-110 hover:bg-[#1E3A8A] hover:text-white absolute right-0"
+            onClick={AbrirModalEdit}
           >
             Editar
           </button>
         </div>
         <div className="w-[90%] my-[2%] flex relative">
-          <p className="md:text-[14px] text-[12px] text-justify">
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla
-            facilisi. Sed vitae ligula id justo sodales varius. Fusce fermentum,
-            lacus at fermentum luctus, nisl purus fringilla enim, vel
-            scelerisque lorem urna ut libero. Suspendisse potenti. Aenean at
-            metus at augue laoreet scelerisque. Integer et justo at lorem
-            efficitur tincidunt. Curabitur nec magna ut libero tincidunt
-            faucibus.Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-            Nulla facilisi. Sed vitae ligula id justo sodales varius. Fusce
-            fermentum, lacus at fermentum luctus, nisl purus fringilla enim, vel
-            scelerisque lorem urna ut libero. Suspendisse potenti. Aenean at
-            metus at augue laoreet scelerisque. Integer et justo at lorem
-            efficitur tincidunt. Curabitur nec magna ut libero tincidunt
-            faucibus.
-          </p>
+          <p className="md:text-[14px] text-[12px] text-justify">{desc}</p>
         </div>
 
         {/* Minhas Informações */}
         <h1 className="font-bold md:text-[40px] text-2xl mt-[3%] mb-[1%]">
           Minhas Informações
         </h1>
-        <hr className="w-full border-[#274CB4] border-1 mb-[2%]"/>
-        <MyInformations></MyInformations>
+        <hr className="w-full border-[#274CB4] border-1 mb-[2%]" />
+        <MyInformations
+          email={emailExibi}
+          insta={insta}
+          tel={tel}
+          doacao={doacao}
+          loc={loc}
+        ></MyInformations>
 
         {/* Minhas Postagens */}
         <h1 className="font-bold md:text-[40px] text-2xl mt-[3%] mb-[1%]">
           Minhas Postagens
         </h1>
-        <hr className="w-full border-[#274CB4] border-1 mb-[2%]"/>
+        <hr className="w-full border-[#274CB4] border-1 mb-[2%]" />
       </div>
 
       {/* Modal Post*/}
@@ -102,6 +124,34 @@ export default function Profile() {
             onClick={AbrirModalPost}
           ></div>
           <AddPost abrirModal={AbrirModalPost}></AddPost>
+        </div>
+      )}
+
+      {/* Modal Edit*/}
+      {modalEdit && (
+        <div className="absolute z-80 w-full h-full flex items-center justify-center">
+          <div
+            className="absolute w-full h-full bg-[rgba(0,0,0,0.7)] bg-opacity-30 cursor-pointer"
+            onClick={AbrirModalEdit}
+          ></div>
+          <EditProfile
+            abrirModal={AbrirModalEdit}
+            image={image}
+            userExi={userExi}
+            username={username}
+            insta={insta}
+            tel={tel}
+            doacao={doacao}
+            loc={loc}
+            emailExi={emailExibi}
+            setImage={setImage}
+            setUserExibi={setUserExi}
+            setInsta={setInsta}
+            setTel={setTel}
+            setDoacao={setDoacao}
+            setLoc={setLoc}
+            setEmailExi={setEmailExibi}
+          ></EditProfile>
         </div>
       )}
     </div>
